7e009faaf1f182f7b8dc037a7bcdbcad5f58e769
gwhenry <gwei1016@gmail.com>
2014-12-01 17:21:14 +0800

    mod code

--- mis/application/models/carpool_model.php
+++ mis/application/models/carpool_model.php
@@ -27,4 +27,26 @@ class Carpool_Model extends CI_Model{
 		$query = $this->db->query("update user_info  set status =2 where user_id = $user_id");
 		return 0;
 	}
+
+	public function get_order_list($start, $end){
+		$res = array();		
+		$query = $this->db->query("select item1 from log_info where day > $start and day < $end ");
+		foreach ($query->result_array() as $row)$res[]=$row;		
+        return $res; 
+    }
+
+    public function get_succ_order_list($start, $end){
+		$res = array();		
+		$query = $this->db->query("select item2 from log_info where day > $start and day < $end ");
+		foreach ($query->result_array() as $row)$res[]=$row;		
+        return $res; 
+    }
+
+    public function get_timeout_order_list($start, $end){
+		$res = array();		
+		$query = $this->db->query("select item3 from log_info where day > $start and day < $end ");
+		foreach ($query->result_array() as $row)$res[]=$row;		
+        return $res; 
+    }
+
 }
