763c2e4a65216b6c7d4c7e74508c811d6e1ee468
gwhenry <gwei1016@gmail.com>
2014-12-05 14:09:16 +0800

    mod code

--- mis/application/controllers/driver.php
+++ mis/application/controllers/driver.php
@@ -14,18 +14,32 @@ class Driver extends MY_Controller {
 		$this->data['js'][] = "js/jquery.typist";
 		$this->load->view('include/header',$this->data);
 		$this->load->view('driver/list');   
-		$this->load->view('include/footer');
-		$this->get_list(0, 10);
+		$this->load->view('include/footer');		
+        if(!isset($_GET['page'])) {
+            $page = 0;
+        }else{
+            $page = intval($_GET['page']);
+        }
+        
+        $this->get_list( $page * 10, 10);
 	}
     public function get_list($start , $limit){
        $this->load->model("carpool_model"); 
        $list = $this->carpool_model->get_driver($start , $limit);
        $data["list"] = $list;
+       $data["page"] = $start/10;
        $this->load->view('driver/driver',$data);
     }
 	public function test(){
 	}
-    public function set($user_id){
+    public function set(){
+        $this->load->model("carpool_model"); 
+        $this->carpool_model->set_driver(intval($_POST['user_id']));
+        echo json_encode(array(
+            'errno' => 0,
+            'errmsg'=> '',
+        ));
+        return;
     }
 
 	public function log(){
--- mis/application/views/driver/driver.php
+++ mis/application/views/driver/driver.php
@@ -1,19 +1,24 @@
 <script type="text/javascript"> 
     function Click(obj)
     {
-        alert(obj);  //单击事件完成的功能－输出提示
-        $.post(base_url+"?c=driver&m=set",{user_id:obj},function(data){
-            if(data.errno == 0){
-                return;
-            }else{
-                $('#message').html(data.errmsg);
-                $('#messagebox').modal('show');
-                return;
-            }
+        $.post("http://"+window.location.host+"/mis/index.php?c=driver&m=set",{user_id:obj},function(data){
+            location.reload(true);
+            return;
         });
+        
         
     }
 </script>
+
+<div  class="button">
+<?php if ($page != 0):?>
+             
+    <a href="<?php echo base_url();?>mis/index.php?c=driver&m=index&page=<?php echo $page-1;?>"   class="btn btn-success" >上一页</a>
+
+<?php endif;?> 
+             
+    <a href="<?php echo base_url();?>mis/index.php?c=driver&m=index&page=<?php echo $page+1;?>"   class="btn btn-success" >下一页</a>
+</div>
 <?php if (!empty($list)):?>
 	<?php foreach ($list as $item): ?>
 		<div  class="list-group-item">
@@ -24,12 +29,12 @@
                 <p><i class="icon-star">状态： <?php if($item['status'] == 0)echo '未审核';else echo '已审核';?></i></p>
             </div>   
             <div class="button" style="margin-bottom: 0px;">               
-                <input type="button" id=<?php echo "go".$item['user_id'];?> onclick="Click(<?php echo $item['user_id'];?>)" value="通过"> 
+                <input type="button" id=<?php echo "go".$item['user_id'];?> onclick="Click(<?php echo $item['user_id'];?>)" value="通过" class="btn btn-success"> 
             </div>             
         </div>  
 	<?php endforeach;?>
 <?php else:?>
-    暂时没有司机    
+    没有数据了    
 <?php endif;?>
 
 
