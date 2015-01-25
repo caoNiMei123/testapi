07580cd17217c5ac556ffac78acefadf9032c11d
gwhenry <gwei1016@gmail.com>
2015-01-25 15:14:18 +0800

    fix bug

--- mis/application/logs/log-2014-11-27.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2014-11-27 16:30:47 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away /home/gaowei/gitcode/trunk/mis/system/database/drivers/mysql/mysql_driver.php 91
-ERROR - 2014-11-27 16:30:54 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away /home/gaowei/gitcode/trunk/mis/system/database/drivers/mysql/mysql_driver.php 91
