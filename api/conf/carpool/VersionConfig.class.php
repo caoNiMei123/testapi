a8370ab7da9d37115a73f8d17ca96d7d01c86bea
gwhenry <gwei1016@gmail.com>
2015-01-31 11:04:35 +0800

    mod version

--- api/conf/carpool/VersionConfig.class.php
+++ api/conf/carpool/VersionConfig.class.php
@@ -1,16 +1,16 @@
 <?php
 class VersionConfig{
     public static $driver_version = array(
-       'version' =>  '0.0.0.9',
-       'url' => 'xxxx',
-       'detail' => 'xxxx',
-       'force_update' => true,
+       'version' =>  '0.0.9',
+       'url' => 'http://182.92.164.183:8089/pc_driver_v0.94.apk',
+       'detail' => 'driver',
+       'force_update' => false,
     );
     public static $passenger_version = array(
-       'version' =>  '0.0.0.9',
-       'url' => 'xxxx',
-       'detail' => 'xxxx',
-       'force_update' => true,
+       'version' =>  '0.0.9',
+       'url' => 'http://182.92.164.183:8089/pc_driver_v0.94.apk',
+       'detail' => 'passgenger',
+       'force_update' => false,
     );
 
 
