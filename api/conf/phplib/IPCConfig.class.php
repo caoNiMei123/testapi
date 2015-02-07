9c3f9f7e240a802b8128698792bd45b20c2c33ca
gwhenry <gwei1016@gmail.com>
2015-02-07 18:20:34 +0800

    fix

--- api/conf/phplib/IPCConfig.class.php
+++ api/conf/phplib/IPCConfig.class.php
@@ -6,7 +6,7 @@ class IPCConfig
         'machine' => '/tmp/tmp_socket',
         'connect_timeout' => 1000,
         'timeout' => 10000,
-    )
+    );
 }
 
 /* vim: set ts=4 sw=4 sts=4 tw=100 noet: */
