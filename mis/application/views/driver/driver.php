4797c2e9bb6c8f808dd3fe33c8b304fc05cc1c69
gwhenry <gwei1016@gmail.com>
2015-04-18 16:50:19 +0800

    fix bug

--- mis/application/views/driver/driver.php
+++ mis/application/views/driver/driver.php
@@ -1,11 +1,11 @@
 <script type="text/javascript"> 
-    function Click(ph ,obj, st, ct, cn)
-    {   
-        var value1 = document.getElementById("ct").value;
-        alert(value1);
-        var value2 = document.getElementById("cn").value;
-        alert(value2);
-        $.post("http://"+window.location.host+"/mis/index.php?c=driver&m=set",{phone:ph,user_id:obj, status:st, car_type:value1, car_num:value2},function(data){
+    function Click(ph ,obj, st)
+    {  
+	var key1 = "car_type_"+obj; 
+        var ct = document.getElementById(key1).value;
+	var key2 = "car_num_"+obj;
+	var cn = document.getElementById(key2).value;
+	$.post("http://"+window.location.host+"/mis/index.php?c=driver&m=set",{phone:ph,user_id:obj, status:st, car_type:ct, car_num:cn},function(data){
             location.reload(true);
             return;
         });
@@ -30,8 +30,8 @@
                <p><i class="icon-time">创建时间： <?php echo date("Y-m-d",$item['ctime']);?></i></p>
                 <p><i class="icon-star">手机号： <?php echo $item['phone'];?></i></p>
                 <form method="post" action="">
-                <p>车  型： <input type="text" name="<?php echo "car_type_".$item['user_id'];?>" ></p>
-                <p>车牌号：<input type="text" name="<?php echo "car_num_".$item['user_id'];?>" ></p>
+                <p>车  型： <input type="text" id="<?php echo "car_type_".$item['user_id'];?>" ></p>
+                <p>车牌号：<input type="text" id="<?php echo "car_num_".$item['user_id'];?>" ></p>
                 </form>
             </div>   
             <div >               
@@ -39,8 +39,8 @@
                 <img src="<?php echo $item['licence_url']?>"  alt="行驶证" style="width:150px;height:150px;"/>
             </div>             
             <div class="button" style="margin-bottom: 0px;">               
-                <input type="button" id=<?php echo "go_suc".$item['user_id'];?> onclick="Click(<?php echo $item['phone'];?>,<?php echo $item['user_id'];?>,2,<?php echo "car_type_".$item['user_id'];?>,<?php echo "car_num_".$item['user_id'];?>)" value="通过" class="btn btn-success"> 
-                <input type="button" id=<?php echo "go_fail".$item['user_id'];?> onclick="Click(<?php echo $item['phone'];?>,<?php echo $item['user_id'];?>,3,<?php echo "car_type_".$item['user_id'];?>,<?php echo "car_num_".$item['user_id'];?>)" value="拒绝" class="btn btn-success"> 
+                <input type="button" id=<?php echo "go_suc".$item['user_id'];?> onclick="Click(<?php echo $item['phone'];?>,<?php echo $item['user_id'];?>,2)" value="通过" class="btn btn-success"> 
+                <input type="button" id=<?php echo "go_fail".$item['user_id'];?> onclick="Click(<?php echo $item['phone'];?>,<?php echo $item['user_id'];?>,3)" value="拒绝" class="btn btn-success"> 
             </div>             
         </div>  
     <?php endforeach;?>
